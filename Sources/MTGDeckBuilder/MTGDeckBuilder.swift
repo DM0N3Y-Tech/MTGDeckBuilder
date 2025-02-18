202e025b0ed2f40e86c0b88cf1cfcfbd1a79a67b
DM0N3Y-Tech <deleon.jorge18@gmail.com>
2025-02-18 01:25:44 -0600

    Update MTGDeckBuilder.swift

--- Sources/MTGDeckBuilder/MTGDeckBuilder.swift
+++ Sources/MTGDeckBuilder/MTGDeckBuilder.swift
@@ -1,7 +1,11 @@
+import ScryfallKit
+
 public struct MTGDeckBuilder {
     public init() {}
-    
-    public func exampleFunction() -> String {
-        return "MTG Deck Builder Loaded!"
+
+    public func fetchCard(named name: String) async throws -> Card? {
+        let api = ScryfallAPI()
+        let cards = try await api.searchCards(query: name)
+        return cards.first
     }
 }
